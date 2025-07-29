8e7292aa37b1798081451a27dfe28bf4a4b552c7
Isa8205 <kibetdavis848@gmmail.com>
2025-07-29 09:52:00 +0300

    Add Counter component using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Counter from './components/Counter';
 
-const App = () => {
+function App() {
   return (
     <div>
-      <Header />
-      <UserProfile />
-      <MainContent />
-      <Footer />
+      <h1>Simple Counter App</h1>
+      <Counter />
     </div>
   );
-};
+}
 
 export default App;
